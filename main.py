5ed0b9c9f3b3f050703d1e7fed1913beba2cb8f0
Zauntres <zauntres@gmx.de>
2024-09-05 22:27:33 +0200

    Word counting works

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ def main():
     book_path = "books/frankenstein.txt"
     text = read_book(book_path)
     print(text)
+    print (count_words(text))
 
 def read_book(path):
     with open(path) as f:
        return f.read()
+    
+def count_words(text):
+    words = text.split()
+    word_count = 0
+    for word in words:
+        word_count += 1
+    return word_count
 
 main()
