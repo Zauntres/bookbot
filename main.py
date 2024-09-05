b20efed4a578d7a51177b7a7444aaa4e24395b5d
Zauntres <zauntres@gmx.de>
2024-09-05 22:33:19 +0200

    Better word counting

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     book_path = "books/frankenstein.txt"
     text = read_book(book_path)
     print(text)
-    print (count_words(text))
+    print (count_words(text), "words found in the dokument")
 
 def read_book(path):
     with open(path) as f:
@@ -10,9 +10,10 @@ def read_book(path):
     
 def count_words(text):
     words = text.split()
-    word_count = 0
-    for word in words:
-        word_count += 1
-    return word_count
+#    word_count = 0
+#   for word in words:
+#        word_count += 1
+#    return word_count
+    return len(words)
 
 main()
