df456af7866e6836dbc37e9e5e13e626070bf716
Zauntres <zauntres@gmx.de>
2024-09-05 23:16:27 +0200

    Counting charcters is hard

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = read_book(book_path)
-    print(text)
+#    print(text)
     print (count_words(text), "words found in the dokument")
+    print(letter_count(text))
 
 def read_book(path):
     with open(path) as f:
@@ -10,10 +11,30 @@ def read_book(path):
     
 def count_words(text):
     words = text.split()
-#    word_count = 0
-#   for word in words:
-#        word_count += 1
-#    return word_count
     return len(words)
+
+def letter_count(text):
+    words = text.split()
+    for word in words:
+        lower_string = word.lower()
+        count = 0
+        alphabet = {""}
+        print(lower_string)
+        for letter in lower_string:
+                count += 1
+                alphabet.add(letter)
+        print(count)
+        print(alphabet)
+
+"""
+charakter = {
+    "a" = 3;
+    "b" = 0,
+    "c" = 0,
+    ...
+}
+print(charakter[a]) --> 3
+"""
+
 
 main()
