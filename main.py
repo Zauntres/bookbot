ff0d1e6884bcac79ab795c896ec1264647f52545
Zauntres <zauntres@gmx.de>
2024-09-09 20:26:42 +0200

    Set full

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = read_book(book_path)
-#    print(text)
+    print(text)
     print (count_words(text), "words found in the dokument")
     print(letter_count(text))
 
@@ -15,25 +15,28 @@ def count_words(text):
 
 def letter_count(text):
     words = text.split()
+    alphabet = {""}
+    characters = {}
     for word in words:
         lower_string = word.lower()
         count = 0
-        alphabet = {""}
         print(lower_string)
         for letter in lower_string:
-                count += 1
-                alphabet.add(letter)
+            count += 1
+            alphabet.add(letter)
+        characters[letter[0]] = count
         print(count)
-        print(alphabet)
+    print(alphabet)
+    print(characters)
 
 """
-charakter = {
+character = {
     "a" = 3;
     "b" = 0,
     "c" = 0,
     ...
 }
-print(charakter[a]) --> 3
+print(character[a]) --> 3
 """
 
 
