778fbc08fc59c8bb00aff1be8e551e55d031ee3f
Zauntres <zauntres@gmx.de>
2024-09-09 20:26:49 +0200

    Set full/bin/python3

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def count_words(text):
 
 def letter_count(text):
     words = text.split()
-    alphabet = {""}
+    alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"} 
     characters = {}
     for word in words:
         lower_string = word.lower()
