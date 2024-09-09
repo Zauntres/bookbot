120662e4a4c90589195d411751036a542edd7fb6
Zauntres <zauntres@gmx.de>
2024-09-09 22:57:49 +0200

    Sorted String

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = read_book(book_path)
-    print(text)
+    #print(text)
+    print(f"--- Begin report of {book_path} ---")
     print (count_words(text), "words found in the dokument")
     print(letter_count(text))
+    print(sorting_characters(letter_count(text)))
 
 def read_book(path):
     with open(path) as f:
@@ -14,7 +16,6 @@ def count_words(text):
     return len(words)
 
 def letter_count(text):
-    #words = text.split()
     characters = {}
     for word in text:
         lower_string = word.lower()
@@ -24,6 +25,15 @@ def letter_count(text):
             characters[lower_string] = 1
     return characters
 
+def sorting_characters(dict):
+    sort_out = dict
+    for chars in dict:
+        char_list = chars.split()
+        for letter in char_list:
+            if letter.isalpha():
+                sort_out[char_list[0]] = dict[chars] 
+    rsort = sorted(sort_out, key=lambda sort_out: sort_out)
+    return rsort
 
 
 main()
