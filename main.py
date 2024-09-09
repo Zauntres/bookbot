5ffa62ef97444cafc10cb72fbcf79eb6e5040adc
Zauntres <zauntres@gmx.de>
2024-09-09 21:29:24 +0200

    abgegelichen mit Solution

--- main.py
+++ main.py
@@ -14,32 +14,16 @@ def count_words(text):
     return len(words)
 
 def letter_count(text):
-    words = text.split()
-    alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"} 
+    #words = text.split()
     characters = {}
-    for word in words:
+    for word in text:
         lower_string = word.lower()
-        count = 0
-        #print(lower_string)
-        for letter in lower_string:
-            print(letter)
-            if letter in alphabet:
-                count += 1
-            #alphabet.add(letter)
-            characters[letter[0]] = count
-       # print(count)
-   # print(alphabet)
-    print(characters)
+        if lower_string in characters:
+            characters[lower_string]+= 1
+        else:
+            characters[lower_string] = 1
+    return characters
 
-"""
-character = {
-    "a" = 3;
-    "b" = 0,
-    "c" = 0,
-    ...
-}
-print(character[a]) --> 3
-"""
 
 
 main()
